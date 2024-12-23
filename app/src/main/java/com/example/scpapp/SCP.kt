e5a060cd253c4fdf33fd80187d04ed065951d863
Martin Lehmden <Extreme69>
2024-12-23 23:08:34 +0100

    Improved SCP model

--- app/src/main/java/com/example/scpapp/SCP.kt
+++ app/src/main/java/com/example/scpapp/SCP.kt
@@ -1,14 +1,15 @@
 package com.example.scpapp
+import com.google.gson.annotations.SerializedName
 
 // SCP model class representing the data structure for SCPs
 data class SCP(
-    val scpId: String,
-    val title: String,
-    val description: String,
-    val classification: String,
-    val photoUrl: String,
-    val rating: Int,
-    val scpTales: Array<String>,
-    val url: String,
-    val series: String
+    @SerializedName("scp_id") val id: String,
+    @SerializedName("title") val title: String,
+    @SerializedName("description") val description: String,
+    @SerializedName("classification") val classification: String,
+    @SerializedName("photo_url") val photoUrl: String,
+    @SerializedName("rating") val rating: Int,
+    @SerializedName("scp_tales") val scpTales: List<String>,
+    @SerializedName("url") val url: String,
+    @SerializedName("series") val series: String
 )
